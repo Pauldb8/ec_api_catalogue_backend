ebe5a50dfab08682b35b4c8ed1b8717c2929a6e3
Paul R. De Buck <ronald-paul.de-buck@ext.ec.europa.eu>
2024-02-22 16:33:38 +0100

    added route apis by id

--- src/routers/apis.ts
+++ src/routers/apis.ts
@@ -1,15 +1,21 @@
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
 import fs from 'fs/promises';
-import { apisJSONPath } from '@lib/config/environment.js';
-
-const apis = await fs.readFile(apisJSONPath, 'utf8').then(s =>
+const apis = await fs.readFile('all-apis.json', 'utf8').then(s =>
 	s
 		.split('\n')
 		.filter(l => l)
 		.map(l => JSON.parse(l))
 );
 const router = Router();
-
-router.route('/').get<unknown, unknown[]>((req, res, next) => res.json(apis));
-
+router.route('/').get((req: Request, res: Response) => res.json(apis));
+router.route('/:apiId').get((req: Request, res: Response) => {
+	const { apiId } = req.params;
+	// Find the API with the matching id
+	const api = apis.find(api => api.id.toString() === apiId);
+	if (api) {
+		res.json(api); // If found, return the API
+	} else {
+		res.status(404).send('API not found'); // If not found, return a 404 error
+	}
+});
 export default router;
