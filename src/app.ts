7f88d2a46f457a4839748851bd300a9b284d7ab9
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-03-04 11:54:23 +0100

    feat(src): Merge index.ts and app.ts, and remove HTTPS server wrapper

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,82 @@
-import { accessLogPath, nodeEnvironment } from '@lib/config/environment.js';
-import { JwtUser } from '@lib/middleware/auth.js';
-import { errorHandler, errorIsUnknownType, notFound } from '@lib/middleware/error.js';
-import Sentry from '@sentry/node';
+import { RewriteFrames } from '@sentry/integrations';
+import * as Sentry from '@sentry/node';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
 import express from 'express';
-import fs from 'fs';
+import fs, { mkdirSync } from 'fs';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
-import apisRouter from './routers/apis.js';
+import { accessLogPath, nodeEnvironment, release, sentryDsn } from './config/environment';
+import { JwtUser } from './middlewares/auth';
+import { errorHandler, errorIsUnknownType, notFound } from './middlewares/error';
+import apisRouter from './routes/apis';
 
 const app = express();
+
+// Sentry initialization with Express integration
+Sentry.init({
+	dsn: sentryDsn,
+	integrations: [
+		new Sentry.Integrations.Http({ tracing: true }),
+		new Sentry.Integrations.Express({ app }),
+		new RewriteFrames({
+			iteratee: frame => {
+				const fileDir = __dirname;
+				// const fileDir = path.dirname(new URL(import.meta.url).pathname);
+				const workingDir = process.cwd();
+				if (!frame.filename) return frame;
+				if (frame.filename.startsWith(fileDir)) frame.filename = '/' + path.relative(fileDir, frame.filename).split(path.sep).join(path.posix.sep);
+				if (frame.filename.startsWith(workingDir)) frame.filename = '/' + path.relative(workingDir, frame.filename).split(path.sep).join(path.posix.sep);
+				return frame;
+			},
+		}),
+	],
+	...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+	tracesSampleRate: 1.0,
+	release: release,
+	environment: nodeEnvironment,
+	enabled: !!nodeEnvironment,
+});
 
 app.use(Sentry.Handlers.requestHandler({ ip: true, user: Object.keys(JwtUser.shape) }));
 app.use(Sentry.Handlers.tracingHandler());
 
+// Morgan logger setup
 if (nodeEnvironment) {
-	await fs.promises.mkdir(path.dirname(accessLogPath), { recursive: true });
+	mkdirSync(path.dirname(accessLogPath), { recursive: true });
 	app.use(
 		morgan('combined', {
 			stream: fs.createWriteStream(accessLogPath, { flags: 'a' }),
 		})
 	);
-} else app.use(morgan('dev'));
+} else {
+	app.use(morgan('dev'));
+}
 
+// Middleware setup
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 
+// Routing setup
 app.use('/apis', apisRouter);
 
+// Error handling
 app.use(notFound);
 app.use(Sentry.Handlers.errorHandler({ shouldHandleError: errorIsUnknownType }));
 app.use(errorHandler);
 
-export default app;
+// Starting the Express server
+app.listen(3000, '0.0.0.0', () => {
+	console.log(`Listening at http://0.0.0.0:3000`);
+	const startupTransaction = Sentry.startTransaction({
+		op: 'startup',
+		name: 'Microservice startup',
+	});
+	startupTransaction.finish();
+});
+
