09261bc79e10caa21466434678a48456e5a1c60e
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-03-15 16:38:42 +0100

    feat(Apis): Add a search parameter to the get /apis endpoint

--- src/routes/apis.ts
+++ src/routes/apis.ts
@@ -1,15 +1,73 @@
 import { Request, Response, Router } from 'express';
 import ApiModel from '../models/apiModel';
+import IApi from '../models/apiModel'; // Import the IApi type
+import { Document } from 'mongoose';
 
 const router = Router();
 
-// GET all APIs
+// searchableFields defines the fields to be searched. The order of the fields determines their priority in the search results.
+const searchableFields = [
+  'name',
+  'context',
+  'provider',
+  'version',
+  'description',
+];
+
+// GET APIs based on a search term across multiple fields
 router.route('/').get(async (req: Request, res: Response) => {
   try {
-    const apis = await ApiModel.find(); // Retrieve all APIs from the database
+    const { search } = req.query as { search?: string };
+
+    // If no search term is provided, return all APIs
+    if (!search) {
+      const apis = await ApiModel.find();
+      return res.json(apis);
+    }
+
+    // Build the query for searching across multiple fields
+    const query = {
+      $or: searchableFields.map(field => ({
+        [field]: { $regex: search, $options: 'i' },
+      })),
+    };
+
+    let apis = await ApiModel.find(query); // Execute the search query
+
+    // Sort the results based on the field importance
+    apis = apis.sort((a, b) => {
+      let aIndex = searchableFields.length;
+      let bIndex = searchableFields.length;
+      searchableFields.forEach((field, index) => {
+        if (
+          (
+            a[
+              field as keyof typeof IApi &
+                keyof Document<unknown, object, typeof IApi>
+            ] as string
+          )
+            .toLowerCase()
+            .includes((search as string).toLowerCase())
+        )
+          aIndex = Math.min(aIndex, index);
+        if (
+          (
+            b[
+              field as keyof typeof IApi &
+                keyof Document<unknown, object, typeof IApi>
+            ] as string
+          )
+            .toLowerCase()
+            .includes((search as string).toLowerCase())
+        )
+          bIndex = Math.min(bIndex, index);
+      });
+      return aIndex - bIndex;
+    });
+
     res.json(apis);
   } catch (error) {
-    console.error('Error retrieving APIs:', error);
+    console.error('Error searching APIs:', error);
     res.status(500).send('Internal Server Error');
   }
 });
