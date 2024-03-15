8c7f710e54906c8d5d411f727629af9d00f89b16
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-03-15 16:39:25 +0100

    feat(MongoDB): Add fileds to the API mongoDB model

--- src/models/apiModel.ts
+++ src/models/apiModel.ts
@@ -1,13 +1,25 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 interface IApi extends Document {
-  id: string;
-  // Define other properties of your API entities here
+  name: string;
+  description: string;
+  context: string;
+  businessOwner: string;
+  technicalOwner: string;
+  version: string;
+  provider: string;
+  openapiDefinition: object;
 }
 
 const ApiSchema: Schema = new Schema({
-  //   id: { type: String, default: () => uuidv4(), unique: true, required: true },
-  // Add other fields here as needed
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  context: { type: String, required: true },
+  businessOwner: { type: String, required: true },
+  technicalOwner: { type: String, required: true },
+  version: { type: String, required: true },
+  provider: { type: String, required: true },
+  openapiDefinition: { type: Schema.Types.Mixed, required: true },
 });
 
 export default mongoose.model<IApi>('Api', ApiSchema);
