eefd56959996c9060eee9a4bca6a073e5ccddfe9
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-03-21 10:51:29 +0100

    fix(SwaggerUI): Change SwaggerUI assets serving to make it available in prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
-const path = require("path");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/app.ts",
-  target: "node",
+  mode: 'production',
+  entry: './src/app.ts',
+  target: 'node',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        './node_modules/swagger-ui-dist/swagger-ui.css',
+        './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
+        './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js',
+        './node_modules/swagger-ui-dist/favicon-16x16.png',
+        './node_modules/swagger-ui-dist/favicon-32x32.png',
+      ],
+    }),
+  ],
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
