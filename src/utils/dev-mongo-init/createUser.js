852926f1bc1b00e102acaaf9145a185e17539852
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-03-25 16:03:58 +0100

    style(Linting): Run lint and format on the whole repo

--- src/utils/dev-mongo-init/createUser.js
+++ src/utils/dev-mongo-init/createUser.js
@@ -3,8 +3,8 @@ db.createUser({
   pwd: process.env.MONGO_INITDB_USER_PASSWORD,
   roles: [
     {
-      role: "readWrite",
-      db: "ec_api_catalogue_backend",
+      role: 'readWrite',
+      db: 'ec_api_catalogue_backend',
     },
   ],
 });
