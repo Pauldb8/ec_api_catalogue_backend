ef23f3d8e2ec5fec310a030cbf05fb853b2782e0
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-04-03 17:44:42 +0200

    refactor(src): Split app.ts into 2 components: app.ts and server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-import { RewriteFrames } from '@sentry/integrations';
-import * as Sentry from '@sentry/node';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import cors from 'cors';
@@ -9,69 +7,13 @@ import helmet from 'helmet';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import path from 'path';
-import {
-  accessLogPath,
-  mongoURI,
-  nodeEnvironment,
-  release,
-  sentryDsn,
-} from './utils/environment';
-import { JwtUser } from './middlewares/auth';
-import {
-  errorHandler,
-  errorIsUnknownType,
-  notFound,
-} from './middlewares/error';
+import { accessLogPath, mongoURI, nodeEnvironment } from './utils/environment';
+import { errorHandler, notFound } from './middlewares/error';
 import apisRouter from './routes/apis';
 import { openapi } from './docs/openapi';
 import swaggerUi from 'swagger-ui-express';
 
 const app = express();
-
-// Sentry initialization with Express integration
-Sentry.init({
-  dsn: sentryDsn,
-  integrations: [
-    new Sentry.Integrations.Http({ tracing: true }),
-    new Sentry.Integrations.Express({ app }),
-    new RewriteFrames({
-      iteratee: frame => {
-        const fileDir = __dirname;
-        // const fileDir = path.dirname(new URL(import.meta.url).pathname);
-        const workingDir = process.cwd();
-        if (!frame.filename) return frame;
-        if (frame.filename.startsWith(fileDir))
-          frame.filename =
-            '/' +
-            path
-              .relative(fileDir, frame.filename)
-              .split(path.sep)
-              .join(path.posix.sep);
-        if (frame.filename.startsWith(workingDir))
-          frame.filename =
-            '/' +
-            path
-              .relative(workingDir, frame.filename)
-              .split(path.sep)
-              .join(path.posix.sep);
-        return frame;
-      },
-    }),
-  ],
-  ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-  tracesSampleRate: 1.0,
-  release: release,
-  environment: nodeEnvironment,
-  enabled: !!nodeEnvironment,
-});
-
-app.use(
-  Sentry.Handlers.requestHandler({
-    ip: true,
-    user: Object.keys(JwtUser.shape),
-  }),
-);
-app.use(Sentry.Handlers.tracingHandler());
 
 // Morgan logger setup
 if (nodeEnvironment) {
@@ -106,17 +48,6 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapi));
 
 // Error handling
 app.use(notFound);
-app.use(
-  Sentry.Handlers.errorHandler({ shouldHandleError: errorIsUnknownType }),
-);
 app.use(errorHandler);
 
-// Starting the Express server
-app.listen(3000, '0.0.0.0', () => {
-  console.log(`Listening at http://0.0.0.0:3000`);
-  const startupTransaction = Sentry.startTransaction({
-    op: 'startup',
-    name: 'Microservice startup',
-  });
-  startupTransaction.finish();
-});
+export default app;
