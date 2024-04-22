6ef65ae2118579299af833c1662b605933c49c51
Thomas Ritaine <thomas.ritaine@ext.ec.europa.eu>
2024-04-22 16:50:14 +0200

    fix(Database): Only create a test database on dev env

--- src/utils/dev-mongo-init/createUser.js
+++ src/utils/dev-mongo-init/createUser.js
@@ -9,18 +9,16 @@ db.createUser({
   ],
 });
 
-testDb = db.getSiblingDB('ec_api_catalogue_backend_test');
-testDb.createUser({
-  user: process.env.MONGO_INITDB_TEST_NAME,
-  pwd: process.env.MONGO_INITDB_TEST_PASSWORD,
-  roles: [
-    {
-      role: 'readWrite',
-      db: 'ec_api_catalogue_backend_test',
-    },
-  ],
-});
-
-console.log(
-  `DEBUG LOG : User ${process.env.MONGO_INITDB_USER_NAME} with password ${process.env.MONGO_INITDB_USER_PASSWORD} created successfully`,
-);
+if (process.env.NODE_ENV === 'development') {
+  testDb = db.getSiblingDB('ec_api_catalogue_backend_test');
+  testDb.createUser({
+    user: process.env.MONGO_INITDB_TEST_NAME,
+    pwd: process.env.MONGO_INITDB_TEST_PASSWORD,
+    roles: [
+      {
+        role: 'readWrite',
+        db: 'ec_api_catalogue_backend_test',
+      },
+    ],
+  });
+}
